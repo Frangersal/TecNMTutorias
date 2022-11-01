6da29bdc6ca16525279f255c8e3546a42fde3916
frangersal <frangersal@outlook.com>
2022-11-01 11:58:47 -0600

    Comment user and question controler

--- app/Http/Controllers/Admin/QuestionsController.php
+++ app/Http/Controllers/Admin/QuestionsController.php
@@ -1,79 +1,75 @@
 <?php
-
+// namespace establece como sera llamado este controlador...
 namespace App\Http\Controllers\Admin;
-
-
+//Llamando a los Modelos 'Form'/Formulario, 'User'/Usuario, 'Question'/Pregunta, 'Option'/Opcion y 'AnswerType'/Tipo de pregunta
 use App\Form;
 use App\User;
 use App\Question;
 use App\Option;
 use App\AnswerType;
-
+//Libreria Controller de Eloquent
 use App\Http\Controllers\Controller;
-
+//Libreria que nos permite obtener objetos de la llamada HTTP
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+    //Funcion para verificar que el usuario haya iniciado sesion
     public function _construct(){
         $this->middleware('auth');
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    //Funcion 'index', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
     public function index()
     {
+        //Guardar en una variable el objeto con toda la informacion de la tabla 'questions' mediante el Modelo 'Question'/Pregunta 
+        //Guardar en una variable el objeto con toda la informacion de la tabla 'answers_types' mediante el Modelo 'AnswerType'/Tipo de pregunta 
+        //Retornar la vista con el objeto almacenado en la variable '$questions','answers_types'
         $questions = Question::all();
-        $answers_types = AnswerType::all();
-        
+        $answers_types = AnswerType::all();        
         return view('admin.questions.index')->with('questions',$questions)->with('answers_types',$answers_types);
     }
+    
+    //Funcion 'create', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+    //Funcion de 'create' descontinuada, quedara comentada para fines educativos. xd
+    //Ha sido reemplazada por la funcion 'show' de este mismo controlador
+    //Por que sigues leyendo?, ya te dije que no se usa...
+    /* public function create() {   echo "<br> -- ".$request; echo "<br> -- ".gettype($request);
+        $answersTypes = AnswerType::all();  $forms = Form::all();
+        return view('admin.questions.create')->with('forms',$forms,)->with('answers_types',$answersTypes,); }*/
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        echo "<br> -- ".$request;
-        echo "<br> -- ".gettype($request);
-        $answersTypes = AnswerType::all();
-        $forms = Form::all();
-        //return view('admin.questions.create')->with('forms',$forms,)->with('answers_types',$answersTypes,);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request answer_type_id
-     * @return \Illuminate\Http\Response
+    /** Funcion 'store', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     *  @param \Illuminate\Http\Request $request, el cual almacena los datos dados en la vista 'show'
+     *  @return object a la vista segun el valor del boton de la vista 'question.show'
      */
     public function store(Request $request)
     {
+        //Variables donde almacenaremos datos del objeto '$request'
+        //Validacion de los datos del request, se pide que el atrbuto 'txtName'
+        //Crear una instancia del objeto Question, con los atributos obtenidos del objeto '$request'
+        //Guardar los datos en la base de datos como un regustro nuevo
         $formId = $request->get('txtIdForm');
         $crear = $request->get('bottonVariable');
-        //echo "<br> -- ". $crear; var_dump($request->all()); //die;
 
         $request->validate([
             'txtName'=>'required',
-            //'txtAnswer_Type'=>'required',
         ]);
 
         $question = new Question([
             'form_id' => $request->get('txtIdForm'),
             'answer_type_id' =>$request->get('txtIdAnswerType'),
             'name' => $request->get('txtName'),
-           // 'option' => $request->get('txtOption'),
         ]);
+
         $question->save();
         
+        //Si el boton de la vista tiene el valor de ´crear´
         if ($crear == "crear") {
+            //Retornar a la vista 'edit' del controlador 'admin.forms' con el parametro del '$formId'
             return redirect()->route('admin.forms.edit',[$formId]);
-
         } elseif ($crear == "crearIrOpcion"){
+            //De lo contrario, si es igual a 'crearIrOpcion', obtener mas parametros
+            //Retornar a la vista 'edit' del controlador 'admin.questions'con el parametro del '$questionId'
             $answer_type_id = $request->get('txtIdAnswerType');
             $name = $request->get('txtName');
             $queryObjQuestion = Question::where('form_id', '=', $formId)->where('answer_type_id', '=', $answer_type_id)->where('name', '=', $name)->first();
@@ -82,49 +78,37 @@ class QuestionsController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
+    /** Funcion 'show', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     *  @param \Illuminate\Http\Request $request, el cual almacena los datos dados en la vista 'forms.edit'
+     *  @return object a la vista segun el Rol del usuario
      */
     public function show($formId)
     {   
-        // formId, tipo de pregunta, 
-        //echo "<br> -- formId: ".$formId; 
-        //echo "<br> --  ".gettype($formId); 
+        // formId, tipo de pregunta,  
         $answersTypes = AnswerType::all(); 
-        $form = Form::where('id', '=', $formId)->first();  
-        //echo "<br> -- form: ".$form; 
+        $form = Form::where('id', '=', $formId)->first(); 
 
         return view('admin.questions.show')->with('form',$form,)->with('answers_types',$answersTypes,);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
+    /** Funcion 'show', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     *  @param \Illuminate\Http\Request $request, el cual almacena los datos dados en la vista 'forms.edit'
+     *  @return object a la vista segun el Rol del usuario
      */
     public function edit(Question $question)    
     {
+        //Creando variables con los paramentros del objeto '$question'
+        //Consultas donde el 'id' de la bd sea igual al 'idForm' que obtuvimos
+        //Consultas donde el 'question_id' de la bd sea igual al 'idQuestion' que obtuvimos
+        //Consulda con todos los registros de 'AnswerType' y 'Question'
         $idForm = $question['form_id'];
         $idQuestion = $question['id'];
-        //echo $idQuestion;
-        //Asignar form a question
         $form = Form::where('id', '=', $idForm)->first();
         $options = Option::where('question_id', '=', $idQuestion)->get(); 
-        //echo $options;
-        //die;
-        
-        //$arrayForm = json_decode($objForm, true);
-        //echo "<br> -- arrayForm: ".$arrayForm['id']; 
-        //echo "<br> -- tipo: ".gettype($arrayForm['id']);
-        //$formId = $arrayForm['id'];
-        //---
         $answers_types = AnswerType::all();
         $questions = Question::all();
 
+        //Retornar a la vista 'questions.edit' con todos los parametros que hemos juntado en las variables
         return view('admin.questions.edit')->with([
             'question'=>$question,
             'answers_types' => $answers_types,
@@ -133,15 +117,17 @@ class QuestionsController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
+    /** Funcion 'update', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     *  @param \Illuminate\Http\Request $request, el cual almacena los datos dados en la vista 'forms.edit'
+     *  @param $id
+     *  @return object a la vista segun el Rol del usuario
      */
     public function update(Request $request, $id)
     {
+        //Variables donde almacenaremos datos del objeto '$request'
+        //Validar propiedades del '$request'
+        //Creamos una instancia de 'Question' que obtendra el registro de la pregunta para actualizarla
+        //Guardamos la pregunta y retornamos a la vista 'forms.edit' con el parametro de '$FormId'
         $editar = $request->get('bottonVariable');
         
         $request->validate([
@@ -156,7 +142,6 @@ class QuestionsController extends Controller
         $question->name = $request->get('name');
         $question->update();
         $formId = $request->get('txtIdForm');
-        
         $question->save();
         return redirect()->route('admin.forms.edit',[$formId]);
     }
@@ -174,16 +159,10 @@ class QuestionsController extends Controller
         //Asignar form a question
         $objForm = Form::where('id', '=', $idForm)->first(); 
         $arrayForm = json_decode($objForm, true);
-        //echo "<br> -- arrayForm: ".$arrayForm['id']; 
-        //echo "<br> -- tipo: ".gettype($arrayForm['id']);
         $formId = $arrayForm['id'];
-        //---
         $answers_types = AnswerType::all();
 
         $question->delete();
-
-        
-        //return redirect()->route('admin.questions.index');
         return redirect()->route('admin.forms.edit',[$formId]);
 
     }
--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -1,99 +1,77 @@
 <?php
-
+// namespace establece como sera llamado este controlador...
 namespace App\Http\Controllers\Admin;
-
+//Libreria Controller de Eloquent
 use App\Http\Controllers\Controller;
+//Libreria DB, util para hacer querys sql crudos
 use Illuminate\Support\Facades\DB;
+//Llamando a los Modelos 'Role'/Rol, 'User'/Usuario, 'Tutor'/Tutor y 'Pupil'/Pupilo
 use App\Role;
 use App\User;
 use App\Tutor;
 use App\Pupil;
+//Llamando 'Gate'/Compuerta para la autorizacion de acciones de distintos usuarios
 use Gate;
+//Libreria que nos permite obtener objetos de la llamada HTTP
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
+    //Funcion para verificar que el usuario haya iniciado sesion
     public function _contruct(){
         $this->middleware('auth');
     }
 
+    //Funcion 'index', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
     public function index()
     {
+        //Guardar en una variable el objeto con toda la informacion de la tabla 'user' mediante el Modelo 'User'/Usuarios 
+        //Retornar la vista con el objeto almacenado en la variable '$users'
         $users = User::all();
         return view('admin.users.index')->with('users', $users);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    //Fuciones creada por defecto y no usada por el momento...
+    // public function create(){ }
+    // public function store(Request $request) { }
+    // public function show(User $user){ }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
+    /** Funcion 'edit', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     *  @param  \App\User $user, Modelo y $user el cual se usara como instancia del modelo
+     *  @return object a la vista segun el Rol del usuario
      */
     public function edit(User $user)
     {
         // Si el usuario en Gate de la accion edit-users no es Admin, entonces solo recargar la pagina para que no tenga acceso
         if(Gate::denies('admin-tutor-action')){
+            //Retornar a la vista 'index' correspondiente a los usuarios con el Rol 'Pupil'/Pupilos o tambien llamados Estudiantes
             return redirect(route('admin.users.index'));
         }
-        #dd($user);
-        $tutors = User::whereHas('roles', function ($query) 
-        {$query->where('name', '=', 'tutor'); })->get();
-
+        //'$tutors' guardara un objeto...
+        //Este, mediante el modelo 'User' obtiene 'whereHas'/'Donde tiene en la tabla relacionada' 'roles'
+        $tutors = User::whereHas('roles', 
+        //El dato 'name' que sea igual a 'tutor' ('whereHas' ocupa esta funcion para obtener el segundo parametro)
+        function ($query) {
+            $query->where('name', '=', 'tutor'); 
+        })->get();
+        //Guardar en una variable '$roles' el objeto con toda la informacion de la tabla 'roles'mediante el Modelo 'Role'/Rol 
+        //Guardar en una variable '$pupils' el objeto con toda la informacion de la tabla 'pupils'mediante el Modelo 'Pupil'/Pupilo 
+        //Retornar a la vista de 'edit' a la que tienen acceso tanto los usuarios con Rol 'Admin'/Administrador, asi como 'Tutor'/Tutor
         $roles = Role::all();
         $pupils = Pupil::all();
         return view('admin.users.edit')->with([
+            //Con los objetos almacenados en las variables que fuimos llenando
             'user'=>$user,'roles'=>$roles,'tutors'=>$tutors,'pupils'=>$pupils
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
+    /** Funcion 'update', puede ser llamada desde web.php como una ruta midiante la llamada de este controlador
+     * @param \Illuminate\Http\Request $request, el cual almacena los datos dados en la vista 'index'
+     * @param \App\User $user, Modelo y $user el cual se usara como instancia del modelo
+     * @return object 
      */
     public function update(Request $request, User $user)
     {
-        #dd($request);
         
         $user->name = $request->name;
         $user->email = $request->email;
@@ -104,7 +82,7 @@ class UsersController extends Controller
 
         $user->roles()->sync($request->roles);
 
-        //Convertir array a string y de ahi a int. xD
+        //Convertir array '$arrayRole' a String y de String a Int
         $arrayRole= $request->roles;
         $stringRole= implode($arrayRole);
         $role = (int)$stringRole;
@@ -112,89 +90,69 @@ class UsersController extends Controller
         //Id del usuario que estamos editando
         $id = $user->id;
         $nada= "puej naah";
-        $tutorDefecto = 1;
-        
-        // $user->save(); 
-        //var_dump($role,$id); die;
+        $tutorDefecto = 1;  
 
-
+        // Segun el numero guardado en la variable '$role' sera la forma en la que continuara...
         switch ($role) {
             case 1:
-                # Admin , no hacer nada
-                // echo "Selecciono D1 <br>";
-                // var_dump($role,$id); 
+                // Caso 1 = Admin; guardar y por ende editar el usuario seleccionado, redirigir a 'index'
                 $user->save(); 
                 return redirect()->route('admin.users.index');
                 break;
             case 2:
-                # Tutor, agregar id a la tabla de tutores, osea ir al metodo crate de tutorescontroller
-                // echo "Selecciono D2 <br>";
-                // var_dump($role,$id); users.tutor.store
-                // necesito tambien el id de user
-                $user->save(); 
-
-                $tutorExist = DB::table('tutors')->whereUser_id($id)->first();                 
-                // var_dump($pupilExist);  die();
+                // Caso 2 = Tutor; guardar y por ende editar el usuario seleccionado,  
+                //Necesitamos saber si el usuario es un 'tutor', consultaremos si su 'id' esta guardado en la tabla 'tutors'
+                //Si la variable '$tutorExist' es nulo, entonces...
+                $user->save();$tutorExist = DB::table('tutors')->whereUser_id($id)->first();
                 if ($tutorExist==null) {
-                    //crear un tutor
-                    $tutor = new Tutor([
-                        'description' => $nada,
-                        'user_id'=> $id,
-                    ]);
-            
-                    $tutor->save();
-
+                    //Crearemos una nueva instancia de Tutor
+                    //Guardar y por ende editar el usuario seleccionado, y retornar a la vista 'index'
+                    $tutor = new Tutor([ 'description' => $nada, 'user_id'=> $id, ]); 
+                    $tutor->save(); 
                     return redirect()->route('users.tutor.index');
                 }
+                //De lo contrario...
                 else {
-                    $tutor = $tutorExist;
-
+                    //En la variable '$tutor' guardamos el objeto con la infromacion usuario 'tutor' de la tabla 'tutors'que habiamos ya guardado en '$tutorExists'
+                    //Retornar a la vista 'index'
+                    $tutor = $tutorExist; 
                     return redirect()->route('users.tutor.index');
-                }
-
-                
-                break;
+                }  break;
             case 3:
-                 # Student, agregar id a la tabla de pupilos, y redireccionar a una vista para seleccionar tutor
-                // echo "Selecciono D3 <br>";
-                // var_dump($role,$id); 
+                // 3 = Student; guardar y por ende editar el usuario seleccionado, 
+                //Necesitamos saber si el usuario es un 'pupil', consultaremos si su 'id' esta guardado en la tabla 'pupils'                  
+                //Si la variable '$pupilExist' es nulo, entonces...
                 $user->save(); 
-
-                $pupilExist = DB::table('pupils')->whereUser_id($id)->first();                 
-                // var_dump($pupilExist);  die();
+                $pupilExist = DB::table('pupils')->whereUser_id($id)->first();
                 if ($pupilExist==null) {
-                    //crear un pupil
-                    // var_dump($pupilExist);
+                    //Crearemos una nueva instancia de Pupil
+                    //Guardar y por ende editar el usuario seleccionado
+                    //Guardar en una variable '$tutors' el objeto con toda la informacion de la tabla 'tutors' mediante el Modelo 'Tutor'/Tutor 
+                    //Retornar a la vista 'edit' con todos los parametros de las variables aca creadas
                     $pupil = new Pupil([
                         'coment' => $nada,
                         'tutor_id'=> $tutorDefecto,
-                        'user_id'=> $id,
-                    ]);
-            
+                        'user_id'=> $id, ]);
                     $pupil->save();
-                    // var_dump($pupil);  die();
-
                     $tutors = Tutor::all();
-        
-                    return view('users.pupil.asignar.edit')->with([
-                        'pupil'=>$pupil,'tutors'=>$tutors,
-                    ]);
+                    return view('users.pupil.asignar.edit')->with([ 'pupil'=>$pupil,'tutors'=>$tutors, ]);
                 }
+                //De lo contrario...
                 else {
-                    $pupil = $pupilExist;
-                    //var_dump($pupil);  die();
-
+                    //En la variable '$pupil' guardamos el objeto con la infromacion usuario 'pupil' de la tabla 'pupils' que habiamos ya guardado en '$pupilExist'
+                    //Guardar en una variable '$tutors' el objeto con toda la informacion de la tabla 'tutors' mediante el Modelo 'Tutor'/Tutor 
+                    //Retornar a la vista 'edit' con todos los parametros de las variables aca creadas
+                    $pupil = $pupilExist; 
                     $tutors = Tutor::all();
-            
                     return view('users.pupil.asignar.edit')->with([
                         'pupil'=>$pupil,'tutors'=>$tutors,
                     ]);
-                }
-
-                break;
+                }  break;
+            //En ninguna circunstancia deberia llegar aca, a menos que haya un error.
             default:
                 // echo "ke pdo?! >:V";
                 // var_dump($role,$id); 
+                //En caso de error redirigira a 'index'
                 return redirect()->route('admin.users.index');
         }
         // echo "ke pdo?! >:V por pinches dos!!!!! >=V";
@@ -208,14 +166,16 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {        
-        // Si el usuario en Gate de la accion edit-users no es Admin, entonces solo recargar la pagina para que no tenga acceso
+        //Si el usuario en Gate de la accion edit-users no es 'Admin', entonces solo recargar la pagina para que no tenga acceso
         if(Gate::denies('admin-tutor-action')){
+            //Retornar a la vista 'index'
             return redirect(route('admin.users.index'));
         }
-
+        //Eliminar el dato de la tabla 'role-user'
+        //Eliminar el dato de la tabla 'user', dato que es dado del parametro de la funcion
+        //Retornar a la vista 'index'
         $user->roles()->detach();
         $user->delete();
-
         return redirect()->route('admin.users.index');
     }
 }
